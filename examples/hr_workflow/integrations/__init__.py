0b2aeade0df3f389e954e4da00671516d8f50e8a
Avishi <109297438+avishi-sreenidhi@users.noreply.github.com>
2026-05-07 15:51:13 +0530

    remove hr_workflow

--- examples/hr_workflow/integrations/__init__.py
+++
@@ -1,6 +0,0 @@
-from .excel_manager import ExcelManager
-from .email_manager import EmailManager
-from .calendar_manager import CalendarManager
-from .hris_mock import HRISMock
-
-__all__ = ["ExcelManager", "EmailManager", "CalendarManager", "HRISMock"]
